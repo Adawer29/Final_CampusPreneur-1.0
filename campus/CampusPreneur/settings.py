a59888f69305ca52f6168df4fe79ff724023cef3
Arnav Dawer <38520361+arnav2907@users.noreply.github.com>
2018-10-20 00:07:01 +0530

    Update settings.py

--- campus/CampusPreneur/settings.py
+++ campus/CampusPreneur/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'accounts',
     'bootstrap3',
     'dashboard',
+    'levels',
 
 
 
@@ -128,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
+MEDIA_URL="/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 LOGIN_REDIRECT_URL='test'
 LOGOUT_REDIRECT_URL='thanks'
